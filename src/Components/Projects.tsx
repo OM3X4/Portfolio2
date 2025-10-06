28469994f4a10cbd86402e13a1f5ec378a1c4533
Omar Emad <om3x4e@gmail.com>
2025-10-06 12:56:14 +0300

    Enhance Projects component: add hover functionality and improve project display

--- src/Components/Projects.tsx
+++ src/Components/Projects.tsx
@@ -1,15 +1,48 @@
 import { motion } from "framer-motion"
+import { useEffect, useState, useRef } from "react"
 import { CUBIC_BEIZER } from "../config"
 import { BiChevronsDown } from "react-icons/bi"
 import type { ProjectType } from "../types"
+import { parseDate } from "../utils"
 
 
 export default function Projects({ projects, changePath }: { projects: ProjectType[], changePath: ({ name, id, color }: { name: string, id: string, color: string }) => void }) {
 
+    const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
+
+    const hoverDateRef = useRef<number | null>(null);
+    const [show, setShow] = useState(false);
+
+    useEffect(() => {
+        if (hoveredIndex === null) {
+            setShow(false);
+            return;
+        }
+
+        const interval = setInterval(() => {
+            if (!hoverDateRef.current) return;
+            const diff = performance.now() - hoverDateRef.current;
+            setShow(diff >= 1000); // after 1s of hover
+        }, 100);
+
+        return () => clearInterval(interval);
+    }, [hoveredIndex]);
+
+    const project = projects[hoveredIndex ?? 0]
+    const data = hoveredIndex !== null ? [
+        <span className="text-xl font-bold">{project.name}</span>,
+        <span>{parseDate(project.date).toLocaleDateString()}</span>,
+        project.mainHeadline.split(" ").slice(0, 6).join(" "),
+        project.mainParagraph.split(" ").slice(0, 6).join(" "),
+        project.quote.split(" ").slice(0, 6).join(" "),
+        project.secondParagraph.split(" ").slice(0, 6).join(" "),
+        <span className="text-8xl">{project.stack[0].icon}</span>,
+        <img src={project.images[0]} alt="" className="absolute inset-0 h-full object-cover object-center"/>
+    ] : null
 
 
     return (
-        <div className="my-20">
+        <div className="my-10">
             <div className="w-fit mx-auto mb-10">
                 <div
                     className="w-fit flex items-center justify-center"
@@ -37,16 +70,18 @@ export default function Projects({ projects, changePath }: { projects: ProjectTy
                     <h4 className="text-muted font-jersey sm:text-2xl">DEVELOPED BY ME</h4>
                 </motion.div>
             </div>
-            <div className="responsive-container grid grid-cols-6 gap-5 mt-10 mb-20">
+            <div className="responsive-container grid grid-cols-3 sm:grid-cols-4 md:grid-cols-6 gap-5 mt-10 mb-20">
                 {
                     projects.map((project: ProjectType, index) => (
                         <motion.div
+                            onMouseEnter={() => { setHoveredIndex(index); hoverDateRef.current = performance.now() }}
+                            onMouseLeave={() => { setHoveredIndex(null); hoverDateRef.current = null }}
                             key={index}
                             initial={{ opacity: 0, y: 160 }}
                             whileInView={{ opacity: 1, y: 0 }}
                             whileHover="hover"
                             variants={{
-                                hover: { scale: 1.05 , backgroundColor: project.backgroundColor}
+                                hover: { scale: 1.05, backgroundColor: project.backgroundColor }
                             }}
                             transition={{ duration: 0.3, ease: CUBIC_BEIZER }}
                             viewport={{ once: true, amount: 0.1 }}
@@ -56,20 +91,38 @@ export default function Projects({ projects, changePath }: { projects: ProjectTy
                             <motion.img
                                 initial={{ filter: "grayscale(1)" }}
                                 variants={{
-                                    hover: { scale: 1.1 , filter: "grayscale(0)" , y: -5 }
+                                    hover: { scale: 1.1, filter: "grayscale(0)", y: -5 }
                                 }}
                                 transition={{ duration: 0.5, ease: CUBIC_BEIZER }}
 
                                 src={project.logo} alt="" className="grayscale-0 object-cover object-center w-1/2" />
                             <motion.h1
-                                // initial={{ opacity: 0, y: 20 }}
                                 variants={{
                                     hover: { opacity: 1, y: -15 }
                                 }}
                                 transition={{ duration: 0.5, ease: CUBIC_BEIZER }}
-                                className="text-2xl sm:text-3xl text-center text-nowrap font-semibold opacity-0 text-white absolute top-0 left-1/2 -translate-x-1/2 z-50">
+                                className="text-2xl sm:text-3xl text-center text-nowrap font-semibold opacity-0 text-white absolute top-0 left-1/2 -translate-x-1/2 z-150">
                                 {project.name}
                             </motion.h1>
+
+                            <div
+                                className="absolute inset-0 w-full h-full pointer-events-none overflow-hidden rounded-2xl"
+                            >
+
+                                <motion.div
+                                    animate={show ? { y: '0%' } : { y: '110%' }}
+                                    transition={{ duration: 0.7, ease: CUBIC_BEIZER }}
+                                    className="w-full h-full grid-inner-block"
+                                >
+                                {
+                                    data && (data.length > index) &&
+                                    <div className="text-white text-center text-large">
+                                        {data[index]}
+                                    </div>
+                                }
+                                </motion.div>
+
+                            </div>
                         </motion.div>
                     ))
                 }
